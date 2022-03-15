46412c72b3457cdaa150a41f70cb1a45566c3d40
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2022-03-15 15:08:24 -0300

    Definido layout de base, creado modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UiSwitchModule } from 'ngx-ui-switch';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LayoutComponent } from './layout/layout.component';
+import { ModalComponent } from './layout/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    UiSwitchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,33 @@
+import { ModalComponent } from './modal/modal.component';
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  sizes: string[] = ['sm', 'md', 'lg'];
 
-  constructor() { }
+  currentSize = this.sizes[1];
+  centered = false;
+  content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta convallis mauris sed auctor. Suspendisse laoreet lacus a magna auctor, vitae suscipit orci commodo.';
 
-  ngOnInit(): void {
+  constructor(
+    private modal: NgbModal
+  ) {}
+
+  ngOnInit(): void { }
+
+  openModal(): void {
+    this.modal.open(ModalComponent, {
+      size: this.currentSize,
+      centered: this.centered,
+    }).result.then((resolve) => {
+      console.log(resolve);
+    }, (reject) => {
+      console.log(reject);
+    });
   }
-
 }
